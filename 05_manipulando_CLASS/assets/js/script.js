7fbeda8edbf98c2af8b829d36b5ea54dd9d73d68
Humberto Teixeira <humberto.quim@gmail.com>
2022-02-18 23:12:51 -0300

    Atualizações do dia 18.02.2022

---
+++ 05_manipulando_CLASS/assets/js/script.js
@@ -0,0 +1,17 @@
+function verde() {
+    //Os comandos abaixo selecionam o elemento #exemplo, entram em sua lista de classes e removem as classes vermelho e azul.
+    
+
+    //Esse comando seleciona o elemento #exemplo, entra em sua lista de classes e adiciona a classe verde.
+    document.querySelector('#exemplo').classList.add('verde');
+}
+
+function vermelho() {
+    //Esse comando seleciona o elemento #exemplo, entra em sua lista de classes e adiciona a classe vermelho.
+    document.querySelector('#exemplo').classList.add('vermelho');
+}   
+
+function azul() {
+    //Esse comando seleciona o elemento #exemplo, entra em sua lista de classes e adiciona a classe azul.
+    document.querySelector('#exemplo').classList.add('azul');
+}
